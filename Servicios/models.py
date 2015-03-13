6cace32945d63d8233601331cfd484dc5f644c14
Raul Esteban Burro Cespedes <ax5bm10@gmail.com>
2015-03-13 11:57:52 -0300

    Creado los modelos del modulo servicio

--- Servicios/models.py
+++ Servicios/models.py
@@ -4,4 +4,11 @@ from Clientes.models import Cliente
 # Create your models here.
 
 class Servicio(models.Model):
-    pass
+    cliente = models.ForeignKey(Cliente)
+    servicio = models.ForeignKey(Service)
+
+class Service(models.Model):
+    titulo = models.CharField(max_length=100)
+    definicion = models.TextField(max_length=1000)
+    precio = models.IntegerField()
+    descuento = models.IntegerField()
